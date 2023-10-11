be4bae63d8d02645c1ed548efcb7be7c15f174b3
Ubuntu <ubuntu@ip-172-31-41-84.eu-west-1.compute.internal>
2023-10-11 09:55:02 +0000

    recheck2

--- .github/workflows/test_file.py
+++ .github/workflows/test_file.py
@@ -1,10 +1,12 @@
 import pytest
 
 
+
 def test_calc_addition():
   # Fonction test du résultat de 2+4
     output = 2+4
     assert output == 6
+
 
 
 def test_calc_substraction():
@@ -13,10 +15,12 @@ def test_calc_substraction():
     assert output == -2
 
 
+
 def test_calc_multiply():
   # Fonction test du résultat de 2*4
     output = 2*4
     assert output == 8
+
 
 
 def test_coucou():
